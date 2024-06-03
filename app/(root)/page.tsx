35a993ca69d76ba28fb6abe123f163c3783eaeea
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-06-03 20:44:29 +0530

    add all files and folder

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+const Home = () => {
+  return (
+    <div>Home
+      {/* <UserButton afterSignOutUrl='/'/> */}
+    </div>
+    
+  )
+}
+
+export default Home
